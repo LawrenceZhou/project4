3243d4213bdbef222f543dc50e294fea48a5a6de
LawrenceZhou <hmskyijun@gmail.com>
2017-02-09 02:39:58 -0800

    2.2

--- components/states/statesController.js
+++ components/states/statesController.js
@@ -8,5 +8,6 @@ cs142App.controller('StatesController', ['$scope', function($scope) {
 
    // Replace this with the code for CS142 Project #4, Problem #2
    console.log('window.cs142models.statesModel()', window.cs142models.statesModel());
+   $scope.states = window.cs142models.statesModel();
 
 }]);
