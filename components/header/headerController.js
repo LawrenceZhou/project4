be28327f41213fb1e58a43011551483362f67240
LawrenceZhou <hmskyijun@gmail.com>
2017-02-11 19:12:14 -0800

    3.5

---
+++ components/header/headerController.js
@@ -0,0 +1,9 @@
+'use strict';
+
+cs142App.controller('HeaderController', ['$scope', function($scope) {
+
+   // $scope.main is defined if we are a child $scope of the main $scope in which
+   // case it contains the page's title property.  We update it so the page title
+   // will include this view's name "Example".
+      $scope.style = 'JAPONESQUE';
+}
